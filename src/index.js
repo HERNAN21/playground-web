c490278367343fb0c9e6472b9a9e6156ec572bdf
Hernan <hernanrojasutani@gmail.com>
2019-09-16 10:37:57 -0500

    Cambio index

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "react-notification-alert/dist/animate.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
-import "../assets/vendor/fullcalendar/dist/fullcalendar.min.css";
-import "../assets/vendor/sweetalert2/dist/sweetalert2.min.css";
-import "../assets/vendor/select2/dist/css/select2.min.css";
-import "../assets/vendor/quill/dist/quill.core.css";
-import "../assets/vendor/nucleo/css/nucleo.css";
-import "../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
-import "../assets/scss/argon-dashboard-pro-react.scss?v1.0.0";
-import "../assets/css/custom-ransa.css";
+import "assets/vendor/fullcalendar/dist/fullcalendar.min.css";
+import "assets/vendor/sweetalert2/dist/sweetalert2.min.css";
+import "assets/vendor/select2/dist/css/select2.min.css";
+import "assets/vendor/quill/dist/quill.core.css";
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/argon-dashboard-pro-react.scss?v1.0.0";
+import "assets/css/custom-ransa.css";
 
 import AdminLayout from "layouts/Admin.jsx";
 import AuthLayout from "layouts/Auth.jsx";
